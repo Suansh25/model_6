7f1c2922005316b1c3fe8eedb5bd1b9c15d61b7d
suansh25 <kurakulasuansh@gmail.com>
2025-08-05 17:10:44 +0530

    adding comments

--- dr_detector_xgboost2.py
+++ dr_detector_xgboost2.py
@@ -12,8 +12,7 @@ from skimage.measure import shannon_entropy
 from skimage.feature import graycomatrix, graycoprops, local_binary_pattern
 from skimage.filters import frangi
 from xgboost import XGBClassifier
-
-# ------------------------- Feature Extraction -------------------------
+#feature extraction feature using the following various features and methods
 def extract_features(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
